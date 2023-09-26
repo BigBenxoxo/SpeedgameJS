4d79b1dd45a507b35387d69de4f4379ee969c72d
BenPet <bendzuke@gmail.com>
2023-09-26 13:54:39 +0300

    updates

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ const endButton = document.querySelector('#endButton')
 const circles = document.querySelectorAll('.circle')
 const scoreDisplay = document.querySelector('.score')
 
+// Global variables 
 let score = 0;
 let timer;
-
+let pace = 1000;
+let active = 0;
+let rounds = 0;
+// Global variables
 
   const getRndInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,7 +20,10 @@ let timer;
 
 
 const clickCircle = (i) => {
-    console.log('Circle was clicked', i);
+if (i !== active) {
+   return endGame()
+}
+rounds--
     score += 10
     scoreDisplay.textContent = score;
 }
@@ -24,21 +31,49 @@ const clickCircle = (i) => {
 circles.forEach((circle, i) => {
     circle.addEventListener('click', () => clickCircle(i))
 })
-
+const enableEvents = () => {
+    circles.forEach(circle => {
+        circle.style.pointerEvents = "auto"
+    })
+}
 
 const startGame = () => {
-    const newActive = pickNew()
-
-    timer = setTimeout(startGame, 1000)
-    
-    function pickNew() {
-      const newActive = getRndInt(0, 3)
-        return console.log(newActive)
+    if (rounds >= 3){
+        return endGame()
     }
+
+
+enableEvents();
+    const newActive = pickNew(active)
+
+circles[newActive].classList.toggle('active')
+circles[active].classList.remove('active')
+
+    active = newActive;
+
+    timer = setTimeout(startGame, pace)
+
+    pace -= 10
+    rounds++
+    
+    
+    function pickNew(active) {
+      const newActive = getRndInt(0, 3)
+      if (newActive !== active) {
+        return newActive
+      }
+        return pickNew(active)
+    }
+    console.log(active);
 }
 const endGame = () => {
     console.log("Game ended");
     clearTimeout(timer)
+    resetGame();
+}
+
+const resetGame = () => {
+    window.location.reload()
 }
 
 
