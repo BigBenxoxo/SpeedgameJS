9380442f1a8d1b361581c39d84339a894084754f
BenPet <bendzuke@gmail.com>
2023-09-26 12:03:27 +0300

    js update mathrandom

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ const startButton = document.querySelector('#startButton')
 const endButton = document.querySelector('#endButton')
 
 const circles = document.querySelectorAll('.circle')
+const scoreDisplay = document.querySelector('.score')
+
+let score = 0;
+
+
+  const getRndInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+  console.log(getRndInt(0, 3));
+
 
 
 const clickCircle = (i) => {
     console.log('Circle was clicked', i);
+    score += 10
+    scoreDisplay.textContent = score;
 }
 
 circles.forEach((circle, i) => {
